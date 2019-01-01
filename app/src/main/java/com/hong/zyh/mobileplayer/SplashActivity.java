6ced5db1d994a58a7795148ae34f0f371d6e679f
zhangyuhong <289590351@qq.com>
2019-01-01 23:08:42 +0800

    增加页面切换按钮功能

--- app/src/main/java/com/hong/zyh/mobileplayer/SplashActivity.java
+++ app/src/main/java/com/hong/zyh/mobileplayer/SplashActivity.java
@@ -1,13 +1,42 @@
 package com.hong.zyh.mobileplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
-public class SplashActivity extends AppCompatActivity {
+import com.hong.zyh.mobileplayer.activity.MainActivity;
+
+public class SplashActivity extends Activity {
+    //获取当前雷类名
+    private static final String TAG = SplashActivity.class.getSimpleName();
+    private Handler handler =new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMianActivity();
+                Log.d(TAG, "MianActivity开启");
+                finish();
+            }
+        }, 2000);
+    }
+
+    //开启主页面的方法
+    private void startMianActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
     }
 }
---
+++ app/src/main/java/com/hong/zyh/mobileplayer/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.hong.zyh.mobileplayer.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hong.zyh.mobileplayer.R;
+
+/**
+ * Created by shuaihong on 2018/12/31.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
