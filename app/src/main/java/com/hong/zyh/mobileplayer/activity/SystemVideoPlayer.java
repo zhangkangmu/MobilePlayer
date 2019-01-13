a66c6ea287ca9ae90771b0095a68c21bd1ff5982
zhangyuhong <289590351@qq.com>
2019-01-13 14:53:44 +0800

    增加播放页面的控制面板UI以及在系统中找到UI面板的id

--- app/src/main/java/com/hong/zyh/mobileplayer/activity/SystemVideoPlayer.java
+++ app/src/main/java/com/hong/zyh/mobileplayer/activity/SystemVideoPlayer.java
@@ -6,7 +6,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.MediaController;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -17,17 +22,90 @@ import com.hong.zyh.mobileplayer.R;
  * 自己创建的系统播放器
  */
 
-public class SystemVideoPlayer extends Activity {
+public class SystemVideoPlayer extends Activity implements View.OnClickListener{
     private Uri uri;
-    private VideoView system_vedio_player;
     //videopager傳過來的數據
     private String videoName;
+    //通過工具找到的id
+    private VideoView system_vedio_player;
+    private LinearLayout llTop;
+    private TextView tvName;
+    private ImageView ivBattery;
+    private TextView tvSystemTime;
+    private Button btVoice;
+    private SeekBar seekbarVoice;
+    private LinearLayout llBottom;
+    private TextView tvCurrentTime;
+    private SeekBar seekbarVideo;
+    private TextView tvDuration;
+    private Button bthExit;
+    private Button btnVideoPre;
+    private Button btnVideoStartPause;
+    private Button btnVideoNext;
+    private Button btnVideoSwitchScreen;
+
+    /**
+     * Find the Views in the layout
+     * Auto-created on 2019-01-13 14:14:39 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    private void findViews() {
+        system_vedio_player = findViewById(R.id.system_vedio_player);
+        llTop = (LinearLayout)findViewById( R.id.ll_top );
+        tvName = (TextView)findViewById( R.id.tv_name );
+        ivBattery = (ImageView)findViewById( R.id.iv_battery );
+        tvSystemTime = (TextView)findViewById( R.id.tv_system_time );
+        btVoice = (Button)findViewById( R.id.bt_voice );
+        seekbarVoice = (SeekBar)findViewById( R.id.seekbar_voice );
+        llBottom = (LinearLayout)findViewById( R.id.ll_bottom );
+        tvCurrentTime = (TextView)findViewById( R.id.tv_current_time );
+        seekbarVideo = (SeekBar)findViewById( R.id.seekbar_video );
+        tvDuration = (TextView)findViewById( R.id.tv_duration );
+        bthExit = (Button)findViewById( R.id.bth_exit );
+        btnVideoPre = (Button)findViewById( R.id.btn_video_pre );
+        btnVideoStartPause = (Button)findViewById( R.id.btn_video_start_pause );
+        btnVideoNext = (Button)findViewById( R.id.btn_video_next );
+        btnVideoSwitchScreen = (Button)findViewById( R.id.btn_video_switch_screen );
+
+        btVoice.setOnClickListener( this );
+        bthExit.setOnClickListener( this );
+        btnVideoPre.setOnClickListener( this );
+        btnVideoStartPause.setOnClickListener( this );
+        btnVideoNext.setOnClickListener( this );
+        btnVideoSwitchScreen.setOnClickListener( this );
+    }
+
+    /**
+     * Handle button click events
+     * Auto-created on 2019-01-13 14:14:39 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    @Override
+    public void onClick(View v) {
+        if ( v == btVoice ) {
+            // Handle clicks for btVoice
+        } else if ( v == bthExit ) {
+            // Handle clicks for bthExit
+        } else if ( v == btnVideoPre ) {
+            // Handle clicks for btnVideoPre
+            //播放的id
+        } else if ( v == btnVideoStartPause ) {
+            // Handle clicks for btnVideoStartPause
+        } else if ( v == btnVideoNext ) {
+            // Handle clicks for btnVideoNext
+        } else if ( v == btnVideoSwitchScreen ) {
+            // Handle clicks for btnVideoSwitchScreen
+        }
+    }
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system_video_player);
-        system_vedio_player = findViewById(R.id.system_vedio_player);
+        //找出activity_system_video_player.xml的id
+        findViews();
+
         videoName = getIntent().getStringExtra("videoName");
 
         //准备好了的监听
@@ -44,8 +122,9 @@ public class SystemVideoPlayer extends Activity {
         if (uri != null) {
             system_vedio_player.setVideoURI(uri);
         }
-        //設置控制面板
-        system_vedio_player.setMediaController(new MediaController(this));
+        //设置控制面板
+        //system_vedio_player.setMediaController(new MediaController(this));
+        //自己定义一個面板
     }
 
     /**
